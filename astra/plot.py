b211ddc52d8b2836dd22e6dbe64df76e1a2f7b05
Fuhao Ji <fuhaoji@hotmail.com>
2020-08-22 00:58:52 -0700

    hack_plot_fieldmaps

--- astra/plot.py
+++ astra/plot.py
@@ -20,27 +20,27 @@ import matplotlib.pyplot as plt
 def old_plot_fieldmaps(astra_input, sections=['cavity', 'solenoid'], fieldmap_dict = {}, verbose=False):
     """
     Plots Cavity and Solenoid fielmaps.
-    
+
     TODO: quadrupoles
-    
+
     """
-    
+
     if fieldmap_dict:
         fmaps = fieldmap_dict
     else:
         fmaps = load_fieldmaps(astra_input, sections=sections, verbose=verbose)
 
     assert len(sections) == 2, 'TODO: more general'
-    
+
     fig, ax0 = plt.subplots()
-    
-    # Make RHS axis for the solenoid field. 
-    ax1 = ax0.twinx()  
+
+    # Make RHS axis for the solenoid field.
+    ax1 = ax0.twinx()
     ax = [ax0, ax1]
-    
+
     ylabel = {'cavity': '$E_z$ (MV/m)', 'solenoid':'$B_z$ (T)'}
     color = {'cavity': 'green', 'solenoid':'blue'}
-    
+
     for i, section in enumerate(sections):
         a = ax[i]
         ixlist = find_fieldmap_ixlist(astra_input, section)
@@ -51,33 +51,68 @@ def old_plot_fieldmaps(astra_input, sections=['cavity', 'solenoid'], fieldmap_di
             a.plot(*dat.T, label=label, color=c)
         a.set_ylabel(ylabel[section])
     ax0.set_xlabel('$z$ (m)')
-    
 
-    
+def hack_plot_fieldmaps(astra_input, sections=['cavity', 'solenoid'], fieldmaps = {}, verbose=False):
+    """
+    Plots Cavity and Solenoid fielmaps.
+    return: plot figure and axis objects
+
+    TODO: quadrupoles
+
+    """
+    fmaps = load_fieldmaps(astra_input, sections=sections, verbose=verbose)
+
+    assert len(sections) == 2, 'TODO: more general'
+#     plt.figure(figsize = (12,8))
+    fig, ax0 = plt.subplots(figsize = (20,4))
+    # Make RHS axis for the solenoid field.
+    ax1 = ax0.twinx()
+    ax = [ax0, ax1]
+
+    ylabel = {'cavity': '$E_z$ (MV/m)', 'solenoid':'$B_z$ (T)'}
+    color = {'cavity': 'green', 'solenoid':'blue'}
+
+    for i, section in enumerate(sections):
+        a = ax[i]
+        ixlist = find_fieldmap_ixlist(astra_input, section)
+        for ix in ixlist:
+            dat = fieldmap_data(astra_input, section=section, index=ix, fieldmaps=fmaps, verbose=verbose)
+            label = f'{section}_{ix}'
+            c = color[section]
+            dat2plot = dat.T
+#             a.plot(*dat.T, label=label, color=c)
+            a.plot(dat2plot[0],abs(dat2plot[1]), label=label, color=c)
+        a.set_ylabel(ylabel[section])
+    ax0.set_xlabel('$z$ (m)')
+
+    # TODO: is this the correct thing to return?
+    return fig, ax
+
+
 def add_fieldmaps_to_axes(astra_object, axes, bounds=None,
                            sections=['cavity', 'solenoid'],
                           include_labels=True):
     """
     Adds fieldmaps to an axes.
-    
+
     """
 
     astra_input = astra_object.input
-    
+
     verbose=astra_object.verbose
-    
+
     if astra_object.fieldmap:
         fmaps = astra_object.fieldmap
-    else:    
+    else:
         fmaps = load_fieldmaps(astra_input, sections=sections, verbose=verbose)
     ax1 = axes
-    
-    ax1rhs = ax1.twinx()  
+
+    ax1rhs = ax1.twinx()
     ax = [ax1, ax1rhs]
-    
+
     ylabel = {'cavity': '$E_z$ (MV/m)', 'solenoid':'$B_z$ (T)'}
     color = {'cavity': 'green', 'solenoid':'blue'}
-    
+
     for i, section in enumerate(sections):
         a = ax[i]
         ixlist = find_fieldmap_ixlist(astra_input, section)
@@ -88,11 +123,11 @@ def add_fieldmaps_to_axes(astra_object, axes, bounds=None,
             a.plot(*dat.T, label=label, color=c)
         a.set_ylabel(ylabel[section])
     ax1.set_xlabel('$z$ (m)')
-    
+
     if bounds:
-        ax1.set_xlim(bounds[0], bounds[1])        
-        
-        
+        ax1.set_xlim(bounds[0], bounds[1])
+
+
 def plot_fieldmaps(astra_object, include_labels=True,  xlim=None, figsize=(12,4), **kwargs):
     """
     Simple fieldmap plot
@@ -102,29 +137,29 @@ def plot_fieldmaps(astra_object, include_labels=True,  xlim=None, figsize=(12,4)
 
     add_fieldmaps_to_axes(astra_object, axes, bounds=xlim, include_labels=include_labels,
                            sections=['cavity', 'solenoid'])
-    
+
 
 def plot_stats(astra_object, keys=['norm_emit_x', 'sigma_z'], sections=['cavity', 'solenoid'], fieldmaps = {}, verbose=False):
     """
-    Plots stats, with fieldmaps plotted from seections. 
-    
+    Plots stats, with fieldmaps plotted from seections.
+
     TODO: quadrupoles
-    
+
     """
-    
+
     astra_input = astra_object.input
-    
+
     fmaps = load_fieldmaps(astra_input, sections=sections, verbose=verbose)
-    
+
     assert len(sections) == 2, 'TODO: more general'
-    
+
     nplots = len(keys) + 1
-    
+
     fig, axs = plt.subplots(nplots)
-    
-    # Make RHS axis for the solenoid field. 
-    
-    
+
+    # Make RHS axis for the solenoid field.
+
+
     xdat = astra_object.stat('mean_z')
     xmin = min(xdat)
     xmax = max(xdat)
@@ -132,47 +167,47 @@ def plot_stats(astra_object, keys=['norm_emit_x', 'sigma_z'], sections=['cavity'
         ax = axs[i]
         unit = astra_object.units(key)
         ydat = astra_object.stat(key)
-        
+
         ndat, factor, prefix = nice_array(ydat)
         label = f'{key} ({prefix}{unit})'
         ax.set_ylabel(label)
         ax.set_xlim(xmin, xmax)
         ax.plot(xdat, ndat)
-    
+
     add_fieldmaps_to_axes(astra_object, axs[-1], bounds=(xmin, xmax),
                            sections=['cavity', 'solenoid'],
                           include_labels=True)
-    
-    
-def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['sigma_z'], 
-                           xkey='mean_z', xlim=None, 
-                           nice=True, 
+
+
+def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['sigma_z'],
+                           xkey='mean_z', xlim=None,
+                           nice=True,
                            include_layout=False,
-                           include_labels=True, 
-                           include_particles=True, 
+                           include_labels=True,
+                           include_particles=True,
                            include_legend=True, **kwargs):
     """
     Plots stat output multiple keys.
-    
-    If a list of ykeys2 is given, these will be put on the right hand axis. This can also be given as a single key. 
-    
+
+    If a list of ykeys2 is given, these will be put on the right hand axis. This can also be given as a single key.
+
     Logical switches, all default to True:
         nice: a nice SI prefix and scaling will be used to make the numbers reasonably sized.
-        
+
         include_legend: The plot will include the legend
-        
+
         include_layout: the layout plot will be displayed at the bottom
-        
-        include_labels: the layout will include element labels. 
-        
+
+        include_labels: the layout will include element labels.
+
     Copied almost verbatim from lume-impact's Impact.plot.plot_stats_with_layout
 
     """
     I = astra_object # convenience
-    
+
     if include_layout:
         fig, all_axis = plt.subplots(2, gridspec_kw={'height_ratios': [4, 1]}, **kwargs)
-        ax_layout = all_axis[-1]        
+        ax_layout = all_axis[-1]
         ax_plot = [all_axis[0]]
     else:
         fig, all_axis = plt.subplots( **kwargs)
@@ -190,11 +225,11 @@ def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['
     # No need for a legend if there is only one plot
     if len(ykeys)==1 and not ykeys2:
         include_legend=False
-    
+
     #assert xkey == 'mean_z', 'TODO: other x keys'
-        
+
     X = I.stat(xkey)
-    
+
     # Only get the data we need
     if xlim:
         good = np.logical_and(X >= xlim[0], X <= xlim[1])
@@ -202,11 +237,11 @@ def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['
     else:
         xlim = X.min(), X.max()
         good = slice(None,None,None) # everything
-    
+
     # Try particles within these bounds
     Pnames = []
     X_particles = []
-    
+
     if include_particles:
         try:
             for pname in range(len(I.particles)): # Modified from Impact
@@ -219,32 +254,32 @@ def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['
             Pnames = []
     else:
         Pnames = []
-        
-    # X axis scaling    
+
+    # X axis scaling
     units_x = str(I.units(xkey))
     if nice:
         X, factor_x, prefix_x = nice_array(X)
         units_x  = prefix_x+units_x
     else:
-        factor_x = 1   
-    
+        factor_x = 1
+
     # set all but the layout
     for ax in ax_plot:
-        ax.set_xlim(xlim[0]/factor_x, xlim[1]/factor_x)          
-        ax.set_xlabel(f'{xkey} ({units_x})')    
-    
+        ax.set_xlim(xlim[0]/factor_x, xlim[1]/factor_x)
+        ax.set_xlabel(f'{xkey} ({units_x})')
 
-    # Draw for Y1 and Y2 
-    
+
+    # Draw for Y1 and Y2
+
     linestyles = ['solid','dashed']
-    
+
     ii = -1 # counter for colors
     for ix, keys in enumerate([ykeys, ykeys2]):
         if not keys:
             continue
         ax = ax_plot[ix]
         linestyle = linestyles[ix]
-        
+
         # Check that units are compatible
         ulist = [I.units(key) for key in keys]
         if len(ulist) > 1:
@@ -252,12 +287,12 @@ def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['
                 assert ulist[0] == u2, f'Incompatible units: {ulist[0]} and {u2}'
         # String representation
         unit = str(ulist[0])
-        
+
         # Data
-        data = [I.stat(key)[good] for key in keys]        
-        
-        
-        
+        data = [I.stat(key)[good] for key in keys]
+
+
+
         if nice:
             factor, prefix = nice_scale_prefix(np.ptp(data))
             unit = prefix+unit
@@ -270,17 +305,17 @@ def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['
             ii += 1
             color = 'C'+str(ii)
             ax.plot(X, dat/factor, label=f'{key} ({unit})', color=color, linestyle=linestyle)
-            
+
             # Particles
             if Pnames:
                 try:
                     Y_particles = np.array([I.particles[name][key] for name in Pnames])
-                    ax.scatter(X_particles/factor_x, Y_particles/factor, color=color) 
+                    ax.scatter(X_particles/factor_x, Y_particles/factor, color=color)
                 except:
-                    pass     
-        ax.set_ylabel(', '.join(keys)+f' ({unit})')            
+                    pass
+        ax.set_ylabel(', '.join(keys)+f' ({unit})')
         #if len(keys) > 1:
-        
+
     # Collect legend
     if include_legend:
         lines = []
@@ -289,18 +324,18 @@ def plot_stats_with_layout(astra_object, ykeys=['sigma_x', 'sigma_y'], ykeys2=['
             a, b = ax.get_legend_handles_labels()
             lines += a
             labels += b
-        ax_plot[0].legend(lines, labels, loc='best')        
-    
-    # Layout   
+        ax_plot[0].legend(lines, labels, loc='best')
+
+    # Layout
     if include_layout:
-        
+
         # Gives some space to the top plot
-        #ax_layout.set_ylim(-1, 1.5)          
-        
+        #ax_layout.set_ylim(-1, 1.5)
+
         if xkey == 'mean_z':
             #ax_layout.set_axis_off()
             ax_layout.set_xlim(xlim[0], xlim[1])
         else:
             ax_layout.set_xlabel('mean_z')
             xlim = (0, I.stop)
-        add_fieldmaps_to_axes(I,  ax_layout, bounds=xlim, include_labels=include_labels)      
+        add_fieldmaps_to_axes(I,  ax_layout, bounds=xlim, include_labels=include_labels)
